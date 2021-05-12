e4d29b93ee62d87209dc688f64b875555fce0080
Dogukan Matuloglu <dogukan.matul@gmail.com>
2021-05-13 02:49:33 +0300

    DB First Done

---
+++ EF-DBFIRST-MOVİES/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_DBFIRST_MOVİES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Actor actor = new Actor() { LanguageID=1,Name="Kemal",Surname="Sunal"};
+            MoviesEntities entities = new MoviesEntities();
+            entities.Actor.Add(actor);
+            entities.SaveChanges();
+
+
+        }
+    }
+}
